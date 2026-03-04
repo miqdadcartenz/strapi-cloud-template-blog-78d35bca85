f6c3d42a2fb087b96d7f6ff0023cb5435c7a412c
Miqdad Abdurrohman <miqdadabdurrohman96@gmail.com>
2026-03-05 05:48:57 +0700

    feat: mencoba pasang upload eksternal dari cloud tencent

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,35 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "*.cos.*.myqcloud.com",
+            "*.myqcloud.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "*.cos.*.myqcloud.com",
+            "*.myqcloud.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-tencent-cloud-storage",
+      providerOptions: {
+        SecretId: env("COS_SECRET_ID"),
+        SecretKey: env("COS_SECRET_KEY"),
+        Region: env("COS_REGION"),
+        Bucket: env("COS_BUCKET"),
+        // Opsional: pakai CDN Tencent untuk akses file
+        // CDNDomain: env("COS_CDN_DOMAIN"),
+      },
+    },
+  },
+});
