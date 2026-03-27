71302bf0d1936240cbf77c74ecfa86f0f33e1d8a
Miqdad <miqdad.abdurrohman@cartenz.co.id>
2026-03-27 14:25:58 +0700

    feat: tambah section testimoni dan sertifikat di homepage

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -774,6 +774,14 @@ export interface ApiHomepageHomepage extends Struct.SingleTypeSchema {
       Schema.Attribute.Private;
     produkSection: Schema.Attribute.Component<'home.section-produk', false>;
     publishedAt: Schema.Attribute.DateTime;
+    sertifikatSection: Schema.Attribute.Component<
+      'home.section-sertifikat',
+      false
+    >;
+    testimoniSection: Schema.Attribute.Component<
+      'home.section-testimoni',
+      true
+    >;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
